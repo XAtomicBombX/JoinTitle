5d99d0cf7cb3808c515cf795387bd29c30098485
XFanblueX <3049197701@qq.com>
2022-09-02 09:43:13 +0800

    完成加载配置文件函数及默认配置

--- join_title/__init__.py
+++ join_title/__init__.py
@@ -1,8 +1,23 @@
 from mcdreforged.api.all import *
 
+default_config = {
+    'title' : '§l§f欢迎回到§a服务器',
+    'subtitle' : '',
+    'actionbar' : '',
+    'permissions' : {
+        'title' : 0,
+        'set' : 3
+    }
+}
+
+def load_config(server:PluginServerInterface):
+    global config
+    config = server.load_config_simple("jointitle.json",default_config=default_config)
+
 
 def on_player_join(server):
     pass
 
-def on_load(server):
-    pass
+def on_load(server:PluginServerInterface,old):
+    load_config(server)
+
